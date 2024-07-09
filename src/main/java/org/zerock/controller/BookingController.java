f2d48076510e5c01f280d28699c5f50b314aaa92
smdude <qazcdef23@naver.com>
2024-07-09 19:28:55 +0900

    0709_1928

--- src/main/java/org/zerock/controller/BookingController.java
+++ src/main/java/org/zerock/controller/BookingController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -43,7 +44,8 @@ public class BookingController {
 		}
 		booking.setPrice(10000L);
         booking.setTotal_price(daysBetween * booking.getPrice()); // 총 가격
-
+        
+        log.info("======================이이이이잉~~~~~~~~~book Get" + booking.getTotal_price());
         model.addAttribute("userId", userId);
         model.addAttribute("booking", booking);
         model.addAttribute("boardName", boardName);
@@ -51,13 +53,13 @@ public class BookingController {
 		return "booking/booking";
 	}
 	
-	@PostMapping("/book.do")
-	public String BookStay(HttpServletRequest request, @RequestParam BookingVO booking, CardRequest cardRequest) {
+	 @PostMapping("/book.do")
+	public String BookStay(BookingVO booking, HttpServletRequest request, CardRequest cardRequest) {
 		String userId = (String) request.getSession(false).getAttribute("userId");
 		CardVO card = bookService.getUserCard(userId); // 카드정보 불러오기
 		
-		
-		log.info("=====================" + booking.getTotal_price());
+		log.info("=====================토탈 프라이스" + booking.getTotal_price());
+		log.info(booking); 
 		if(!checkCardRequest(cardRequest, card)) {
 			log.info("입력 값 오류");
 			return "redirect:/book.do"; // 입력 값 오류
@@ -73,6 +75,7 @@ public class BookingController {
 		return "redirect:/bookinglist.do";
 	}
 	
+	
 	private Long checkDaysBetween(String date1, String date2) {
 		Long daysBetween = 0L;
 		LocalDate localDate1 =  LocalDate.parse(date1);
@@ -82,12 +85,15 @@ public class BookingController {
 	}
 	
 	private Boolean checkCardRequest(CardRequest cardRequest, CardVO card) {
-		if(cardRequest.getConfirmPass() == null || !cardRequest.getConfirmPass().equals(card.getCardPass())) 
+		if(cardRequest.getConfirmPass() == null || !cardRequest.getConfirmPass().equals(card.getCardPass())) {
 			return false; // 비밀번호가 올바르지 않음
-		if(cardRequest.getBank() == null || !cardRequest.getBank().equals(card.getBank()))
+		}
+		if(cardRequest.getBank() == null || !cardRequest.getBank().equals(card.getBank())) {
 			return false; // 은행사가 일치하지 않음
-		if(cardRequest.getCardNum() == 0 || !(cardRequest.getCardNum() == (card.getCardNum())))
+		}
+		if(cardRequest.getCardNum() == 0 || !(cardRequest.getCardNum() == (card.getCardNum()))) {
 			return false;
+		}
 		
 		return true;
 	}
